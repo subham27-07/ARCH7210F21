d08af41cc9b4ee991976b51693944584e38155d4
subham27-07 <82700165+subham27-07@users.noreply.github.com>
2021-10-02 20:20:46 -0400

    Update rabbit.py

--- rabbit.py
+++ rabbit.py
@@ -7,4 +7,48 @@ Inspired by the Python Turtle library, Rabbit provides a toolkit to generate dra
 
 # Rabbit
 
-#This is a terst
+#This is a test
+
+import rhinoscriptsyntax as rs
+import math
+import random
+
+#allObjs = rs.AllObjects()
+#rs.DeleteObjects(allObjs)
+
+class Rabbit:
+    # mirror the lines if needed
+    def mirror(objects, st_point, ed_point):
+        for i in range(0, len(objects)):
+            if st_point[i][1] > ed_point[i][1]:
+                rs.MirrorObject(objects[i], [-1000, 0, 0], [1000, 0, 0])
+
+    # Changing the of scale if required
+    def changeScale(points, new_scale):
+        for i in range(0, len(points)):
+            points[i][0] *= new_scale
+            points[i][1] *= new_scale
+
+        return points
+
+    def getColorList(cls):
+        color= {
+        "red" : [255,0,0],
+        "blue" : [0,0,255],
+        "green" : [0,255,0],
+        "yellow" : [255,185,15],
+        "white" : [248,248,255],
+        "coral" : [255,127,80],
+        "gray" : [166,166,166],
+        }
+        return color
+
+
+    # Making curves
+
+    def curve(Val):
+        a = []
+        for x in range (Val):
+            y = math.sin(random.random()*x)
+            pt = rs.AddPoint(x,y,0)
+            a.append(pt)
